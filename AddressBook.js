a394420dc3f527791ebbceca5bfaee923e0e3fd2
LDeepakReddy <l.deepakreddy.com>
2022-04-28 20:13:18 +0530

    ADDED:Regex patterns to validate contact information and ability to throw an error incase of wrong inputs

--- AddressBook.js
+++ AddressBook.js
@@ -1,32 +1,20 @@
-
 // importing another class file to this class file
 const ContactDetails = require("./ContactDetails.js");
 const prompt = require('prompt-sync')();
 
-// ability to add details to ContactDetails class
-var contactInfo = new ContactDetails("Deepak","Reddy","Medak","Hyderabad","Telangana","502303","9182510123"
-                  ,"deepakreddy@gmail.com");
-
-console.log(contactInfo.toString());
-
-
-contactInfo.firstName = "Ashok";
-contactInfo.lastName = "Kumar";
-contactInfo.address = "Khammam";
-contactInfo.city = "Hyderabad";
-contactInfo.state = "Telangana";
-contactInfo.zip = "500048";
-contactInfo.phoneNumber = "8179765258";
-contactInfo.email = "ashokkumar@gmail.com";
-console.log(contactInfo.toString());
 
 //Ability to add user-defined values to the PersonInfoData
-contactInfo.firstName = prompt("Enter First Name : "); 
-contactInfo.lastName = prompt("Enter Last Name : "); 
-contactInfo.address = prompt("Enter Address : "); 
-contactInfo.city = prompt("Enter City : "); 
-contactInfo.state = prompt("Enter State : "); 
-contactInfo.zip = prompt("Enter Zip : "); 
-contactInfo.phoneNumber = prompt("Enter Phone Number : "); 
-contactInfo.email = prompt("Enter Email : "); 
-console.log(contactInfo.toString());
+try {
+    let contactInfo = new ContactDetails();
+    contactInfo.fName = prompt("Enter First Name : ");
+    contactInfo.lName = prompt("Enter Last Name : ");
+    contactInfo.addressName = prompt("Enter Address : ");
+    contactInfo.cityName = prompt("Enter City : ");
+    contactInfo.stateName = prompt("Enter State : ");
+    contactInfo.zipNumber = prompt("Enter Zip : ");
+    contactInfo.phoneNo = prompt("Enter Phone Number : ");
+    contactInfo.emailID = prompt("Enter Email : ");
+    console.log(contactInfo.toString());
+} catch (e) {
+    console.error(e);
+}
