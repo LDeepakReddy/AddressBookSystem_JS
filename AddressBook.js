1b11f343be3703af3b80f7b99d881d38c263666d
LDeepakReddy <l.deepakreddy.com>
2022-04-28 19:31:18 +0530

    ADDED:contacts to  the addressbook system

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,32 @@
-console.log("Welcome to Addressbook system");
+
+// importing another class file to this class file
+const ContactDetails = require("./ContactDetails.js");
+const prompt = require('prompt-sync')();
+
+// ability to add details to ContactDetails class
+var contactInfo = new ContactDetails("Deepak","Reddy","Medak","Hyderabad","Telangana","502303","9182510123"
+                  ,"deepakreddy@gmail.com");
+
+console.log(contactInfo.toString());
+
+
+contactInfo.firstName = "Ashok";
+contactInfo.lastName = "Kumar";
+contactInfo.address = "Khammam";
+contactInfo.city = "Hyderabad";
+contactInfo.state = "Telangana";
+contactInfo.zip = "500048";
+contactInfo.phoneNumber = "8179765258";
+contactInfo.email = "ashokkumar@gmail.com";
+console.log(contactInfo.toString());
+
+//Ability to add user-defined values to the PersonInfoData
+contactInfo.firstName = prompt("Enter First Name : "); 
+contactInfo.lastName = prompt("Enter Last Name : "); 
+contactInfo.address = prompt("Enter Address : "); 
+contactInfo.city = prompt("Enter City : "); 
+contactInfo.state = prompt("Enter State : "); 
+contactInfo.zip = prompt("Enter Zip : "); 
+contactInfo.phoneNumber = prompt("Enter Phone Number : "); 
+contactInfo.email = prompt("Enter Email : "); 
+console.log(contactInfo.toString());
